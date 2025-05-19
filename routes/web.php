0333d6cd1656b9223b3f6510de527c1127a4bacd
José Onésimo Abel Nuvunga <onesimonuvunga@gmail.com>
2025-05-19 10:57:01 +0200

    Remover login automático após registro e ajustar redirecionamento na rota inicial

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,28 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::middleware(['auth', 'role:admin'])->group(function () {
   // Rotas de admin 
 });
 
 Route::middleware(['auth', 'role:teacher'])->group(function () {
-//rotas de professor
+  //rotas de professor
 });
 
 Route::middleware(['auth', 'role:student'])->group(function () {
   // rotas de estudante
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
