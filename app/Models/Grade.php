10286fa5b12a0065bccdae0dc7776cc09e63af29
Denicio Inácio <113514137+dennycio@users.noreply.github.com>
2025-05-21 15:17:45 +0300

    Add models and migrations for Course, Department, Subject, Enrollment, and Grade

---
+++ app/Models/Grade.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Grade extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['enrollment_id', 'subject_id', 'grade'];
+
+    public function enrollment()
+    {
+        return $this->belongsTo(Enrollment::class);
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+}
